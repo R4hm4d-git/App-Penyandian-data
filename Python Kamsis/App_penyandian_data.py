b28b36bc67d9cbbf3a1fff7966a033042a166280
R4hm4d-git <rahmathamdisiregar@gmail.com>
2026-03-20 14:32:38 +0700

    Update App_penyandian_data.py

--- Python Kamsis/App_penyandian_data.py
+++ Python Kamsis/App_penyandian_data.py
@@ -120,7 +120,7 @@ def dual_decrypt(cipher_bytes, key_val, iv):
     return pesan_asli
 
 st.title("🔐 Updated Kriptografi Hibrida")
-st.markdown("Aplikasi simulasi **Enkripsi Berlapis (Cascade CBC-OFB)** yang diamankan dengan pertukaran kunci **RSA**.")
+st.markdown("Aplikasi simulasi **Enkripsi Berlapis (CBC-OFB)** yang diamankan dengan pertukaran kunci **RSA**.")
 
 tab1, tab2 = st.tabs(["🔒 Enkripsi Pesan", "🔓 Dekripsi Pesan"])
 
